fc7577689401bfcf4c0e1a901385b91c432a5381
Rafael Pavão <rip123123456@gmail.com>
2024-05-28 15:57:52 -0300

    16:00

--- main.js
+++ main.js
@@ -27,6 +27,8 @@ function selecaoBebida(id){
     if(document.getElementById('opcao4').classList.contains('pagina__opcoes__opcao--selecionado') || document.getElementById('opcao5').classList.contains('pagina__opcoes__opcao--selecionado') || document.getElementById('opcao6').classList.contains('pagina__opcoes__opcao--selecionado')){
         let selecionado = document.querySelectorAll('.pagina__opcoes')[1].querySelector('.pagina__opcoes__opcao--selecionado').children[4];
         console.log(selecionado);
+        let variavel = document.querySelectorAll('.pagina__opcoes');
+        console.log(variavel);
         document.getElementById('opcao4').classList.remove('pagina__opcoes__opcao--selecionado');
         document.getElementById('opcao5').classList.remove('pagina__opcoes__opcao--selecionado');
         document.getElementById('opcao6').classList.remove('pagina__opcoes__opcao--selecionado');
@@ -81,4 +83,17 @@ function fecharPedido(){
 function cancelar(){
     document.querySelector('body').classList.remove('body--fechar-pedido');
     document.querySelector('.fechar-pedido').classList.add('fechar-pedido--escondido');
+}
+
+function adicionarItem(){
+    let opcoesComida = document.querySelectorAll('.pagina__opcoes')[0];
+    opcoesComida.innerHTML += ` <div class="pagina__opcoes__opcao" id="opcao1">
+    <a onclick="selecaoComida(1)">
+        <img src="./imgs/frango_yin_yang-1.png" width="144px" height="87px">
+    </a>
+    <p class="texto1">Frango Yin Yang</p>
+    <p class="texto2">Um pouco de batata, um pouco de salada</p>
+    <p class="texto3">R$ 14,90 <div class="img-checkmark img-checkmark--normal"><img src="./imgs/checkmark-circle-1.png"></div></p>
+</div>`;
+    console.log(opcoesComida);
 }
